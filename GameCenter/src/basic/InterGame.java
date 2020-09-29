320486aa82df23bc1f2898c448ccd1c6dfc9475e
Yang Hyunil <xguet7@naver.com>
2020-09-29 12:30:41 +0900

    인터페이스 변경되었습니다. scanner 객체를 파라미터로 받아서 사용해주세요. 객체는 닫지 말아주세요.

--- GameCenter/src/basic/InterGame.java
+++ GameCenter/src/basic/InterGame.java
@@ -3,11 +3,13 @@
  */
 package basic;
 
+import java.util.Scanner;
+
 /**
  * @author MIT0
  *
  */
 public interface InterGame {
-	void title(); //해당게임을 소개하는 내용을 출력
-	void play(); //해당게임을 진행
+	void title(Scanner scan ); //해당게임을 소개하는 내용을 출력
+	void play(Scanner scan ); //해당게임을 진행
 }
