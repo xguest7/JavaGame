fac3258e5988a798984039b3f038ea8b4e12fb87
Yang Hyunil <xguet7@naver.com>
2020-09-29 19:53:24 +0900

    1. 개행문자 처리 개선 (개행문자일경우 다시 받기, 아니면 그대로이용)
    2. 각게임에 해당하는 클래스 호출 초심자도 쉽게 이해할수 있도록 switch 구분으로 변경
    
    3. 아직 미구현된 Game클래스일 경우 게임불가 메세지 출력및 재선택 되도록 수정

--- GameCenter/src/basic/Shell.java
+++ GameCenter/src/basic/Shell.java
@@ -17,8 +17,9 @@ public class Shell {
 	
 	static boolean loopCheck(Scanner scan) {
 		System.out.println("다른 게임을 하시겠습니까?( y or n): ");
-		scan.skip("[\\r\\n]+"); //윈도우 엔터입력시 개행문자 \r\n ,참고> 리눅스는 \n
-		String input=scan.nextLine();
+		String input=null;
+		if((input=scan.nextLine()) .equals(""))  //개행문자가 있으면(문자열값은 빈값으로 입력됨) 다시입력, 개행문자 아니면 정상입력
+			input=scan.nextLine();
 		return input.equals("y") ? true : false;
 	}
 		
@@ -31,12 +32,42 @@ public class Shell {
 	
 	static void playGame(String callClassName, Scanner scan) throws Exception {
 		
-		//해당하는 클래스를 이용하여 객체생성
+/*		//해당하는 클래스를 이용하여 객체생성
 		Class<?> c = Class.forName(callClassName);   //<?>는 제한없은 와일드 카드 즉,모든 유형 다 가능
 		InterGame obj = (InterGame) c.newInstance();
 		
 		//게임진행
 		obj.title(scan);
-		obj.play(scan);
+		obj.play(scan);				*/
+		
+		boolean isGame=true;  
+		InterGame obj=null;
+		switch (callClassName){
+			case "basic.Game1" :
+				obj = new Game1(); break;
+			case "basic.Game2" :
+				obj = new Game2(); break;
+//			case "basic.Game3" :						//에러
+//				obj = new Game3(); break;
+//			case "basic.Game4" :						//에러
+//				obj = new Game4(); break;
+//			case "basic.Game5" : 						//현재 미존재
+//				obj = new Game5(); break;
+			case "basic.Game6" :
+				obj = new Game6(); break;
+//			case "basic.Game7" :						//잘못 구현되어 있음
+//				obj = new Game7(); break;		
+			case "basic.Game8" :
+				obj = new Game8(); break;
+			default:
+				System.out.println("실행이 불가능한 게임입니다.");
+				isGame=false;
+				break;
+		}
+		if(isGame) {
+			obj.title(scan);
+			obj.play(scan);	
+		}
+		
 	}
 }
