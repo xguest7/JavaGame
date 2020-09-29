7b933025bfea2ff8399c234f565ee93cd6c0f317
Min Ji Yun <zpdlxlvpfl@naver.com>
2020-09-29 12:42:33 +0900

    game8 수정

--- GameCenter/src/basic/Game8.java
+++ GameCenter/src/basic/Game8.java
@@ -5,36 +5,44 @@ import java.util.Scanner;
 public class Game8 implements InterGame {
 
 	@Override
-	public void title() {
+	public void title(Scanner scan) {
 		System.out.println("Game8 15이상의 홀수입력시 마름모 생성");
 
 	}
 
 	@Override
-	public void play() {
+	public void play(Scanner scan) {
 		System.out.println("(15이상) 홀수입력:");
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		for (int i = 0; i < num; i++) {
-			for (int j = 0; j < num; j++) {
-				if (i <= num / 2) {
-					if (i + j <= num / 2 + 1)
-						System.out.print(" ");
-					else if (j - i >= num / 2 + 1)
-						System.out.print(" ");
-					else
-						System.out.print("*");
-				} else if (i > num / 2) {
-					if (i - j >= num / 2 + 1)
-						System.out.print(" ");
-					else if (i + j >= num / 2 * 3 + 1)
-						System.out.print(" ");
-					else
-						System.out.print("*");
-				}
+	
+		int num = scan.nextInt();
+		if (num>=15 && num%2==1) {
+			for (int i=0;i<num;i++){
+				  for (int j=0; j<num;j++)
+				  {
+				     if (i<=num/2)
+				     {
+				        if (i+j<=num/2-1)
+				           System.out.print(" ");
+				        else if (j-i>=num/2+1) 
+				        	System.out.print(" ");
+				        else
+				           System.out.print("*");
+				     }
+				     else if (i>num/2) 
+				     {
+				        if (i-j>=num/2+1)
+				        	System.out.print(" ");
+				        else if (i+j>=num/2*3+1)
+				        	System.out.print(" ");
+				        else
+				        	System.out.print("*"); 
+				     }
+				  }
+				  System.out.println();
 			}
-			System.out.println();
-		}
+		}else {
+			System.out.println("15이상 홀수 입력");
 
 	}
+}
 }
